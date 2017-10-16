ba553a1ef891cf215a0f99f75c696d796b31f689
Prokti <thibaud.berdin@maison-berdin.com>
2017-10-16 15:48:52 +0200

    upload fichiers random

--- chantiers/models.py
+++ chantiers/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 from profil.models import Profil
 from django.contrib.auth.models import User
+import os, random
 
 # Create your models here.
+
+def renommage(instance, nom):
+    aleatoire = random.random()
+    nom_fichier = os.path.splitext(nom)[0]
+    return "{}--{}--{}".format(instance.id, nom_fichier, aleatoire)
 
 class Chantier(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     description = models.TextField(null=False)
-    ccmi_doc = models.FileField(upload_to='')
-    appel_1_doc = models.FileField(upload_to='')
-    appel_2_doc = models.FileField(upload_to='')
-    appel_3_doc = models.FileField(upload_to='')
-    appel_4_doc = models.FileField(upload_to='')
-    appel_5_doc = models.FileField(upload_to='')
-    reception_doc = models.FileField(upload_to='')
-    plans_os_doc = models.FileField(upload_to='')
+    ccmi_doc = models.FileField(upload_to=renommage, verbose_name="CCMI")
+    appel_1_doc = models.FileField(upload_to=renommage)
+    appel_2_doc = models.FileField(upload_to=renommage)
+    appel_3_doc = models.FileField(upload_to=renommage)
+    appel_4_doc = models.FileField(upload_to=renommage)
+    appel_5_doc = models.FileField(upload_to=renommage)
+    reception_doc = models.FileField(upload_to=renommage)
+    plans_os_doc = models.FileField(upload_to=renommage)
 
 
     #profil = models.ForeignKey(Profil, on_delete=models.CASCADE)
@@ -42,4 +48,6 @@ class Marche(models.Model):
 
     def __str__(self):
         return self.name
+
+
 
